48bf87318ceca581f96371851911a6613f43a503
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-22 15:33:24 +0800

    Automated bulk repository update

---
+++ src/FloorplanServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rimba\Floorplan;
+
+use Rimba\Base\BitesServiceProvider;
+use Filament\Actions\Action;
+use Filament\Support\Facades\FilamentView;
+use Filament\View\PanelsRenderHook;
+
+
+class FloorplanServiceProvider extends BitesServiceProvider
+{
+    protected string $configFile = __DIR__ . '/../config/bites.php';
+    protected string $viewsPath = __DIR__ . '/../resources/views';
+    protected string $iconsPath = __DIR__ . '/../resources/svg';
+
+    protected function bootPackage(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        FilamentView::registerRenderHook(
+            PanelsRenderHook::GLOBAL_SEARCH_BEFORE,
+            fn (): string => Action::make('FloorPlan')
+                ->label('Floor Plan')
+                ->iconButton()
+                ->badge()
+                ->icon('bites-location')
+                ->url(route('filament.staff.pages.floor-plan'))
+                ->toHtml(),
+        );
+
+    }
+    protected function registerPackage(): void
+    {
+        //
+    }
+
+}
