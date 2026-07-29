1940d738a0256c7c206b156367ea25badc261728
Faros <114036707+a-faros@users.noreply.github.com>
2026-07-29 11:00:52 +0800

    Release v1.2.2

--- src/FloorplanServiceProvider.php
+++ src/FloorplanServiceProvider.php
@@ -4,17 +4,16 @@ declare(strict_types=1);
 
 namespace Rimba\Floorplan;
 
-use Rimba\Base\Services\BitesServiceProvider;
 use Filament\Actions\Action;
 use Filament\Support\Facades\FilamentView;
 use Filament\View\PanelsRenderHook;
-
+use Rimba\Base\Services\BitesServiceProvider;
 
 class FloorplanServiceProvider extends BitesServiceProvider
 {
-    protected string $configFile = __DIR__ . '/../config/bites.php';
-    protected string $viewsPath = __DIR__ . '/../resources/views';
-    protected string $iconsPath = __DIR__ . '/../resources/svg';
+    protected string $viewsPath = __DIR__.'/../resources/views';
+
+    protected string $iconsPath = __DIR__.'/../resources/svg';
 
     protected function bootPackage(): void
     {
@@ -31,9 +30,9 @@ class FloorplanServiceProvider extends BitesServiceProvider
         );
 
     }
+
     protected function registerPackage(): void
     {
         //
     }
-
 }
